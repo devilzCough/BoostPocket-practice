9d0cd22314ab1aff4e12ee81657559a80a4e636e
sihyungyou <sihyungyou@gmail.com>
2020-11-20 02:22:31 +0900

    폴더 정리

--- datamodelTest/Model/Models.swift
+++ datamodelTest/Model/Models.swift
@@ -1,12 +1,16 @@
 //
-//  ExchangeRate.swift
+//  DecodedCountry.swift
 //  datamodelTest
 //
-//  Created by sihyung you on 2020/11/19.
+//  Created by sihyung you on 2020/11/18.
 //  Copyright © 2020 SihyungYou. All rights reserved.
 //
 
 import Foundation
+
+struct DecodedCountry: Codable {
+    let identifier: String
+}
 
 struct ExchangeRate: Codable {
     let rates: [String:Float]
